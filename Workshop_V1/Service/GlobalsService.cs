28a42807c6566b64b291561d80052d17e561ff3f
shakedkr <shakedkr@post.bgu.ac.il>
2016-04-04 13:06:07 +0300

    adding missing files

---
+++ Workshop_V1/Service/GlobalsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Query;
+
+namespace Service
+{
+    class GlobalsService
+    {
+        public static ForumQuery forumQuery = new ForumQuery();
+        public static UserQuery userQuery = new UserQuery();
+
+    }
+}
