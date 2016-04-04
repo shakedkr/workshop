7c353df300d3b863ec514ef78134e780980f74ec
shakedkr <shakedkr@post.bgu.ac.il>
2016-04-04 13:04:56 +0300

    Added files via upload
    adding missing files

---
+++ Workshop_V1/Domain/SystemP/ForumSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.SystemP
+{
+    public class ForumSystem
+    {
+        private List<ForumP.Forum> myForums;
+        private List<UserP.User> myUser;
+        private UserP.User superAdmin;
+
+
+        public ForumSystem()
+        {
+            myForums = new List<ForumP.Forum>();
+            myUser = new List<UserP.User>();
+            superAdmin = new UserP.User();
+        }
+
+        public List<ForumP.Forum> getForums (){
+            return myForums;
+        }
+
+        public List<UserP.User> getUsers()
+        {
+            return myUser;
+        }
+
+    }
+}
